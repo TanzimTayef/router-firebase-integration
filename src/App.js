9edb05a0b4dcab0ea76ff5c8e09990f8980e8909
TanzimTayef <tayef464@gmail.com>
2022-04-09 22:44:12 +0600

    used protected route

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
-import Header from './components/Header/Header';
-import Registers from './components/Registers/Registers';
-import Products from './components/Products/Products';
+import Header from "./components/Header/Header";
+import Registers from "./components/Registers/Registers";
+import Products from "./components/Products/Products";
+import Orders from "./components/Orders/Orders";
+import RequireAuth from "./components/RequireAuth/RequireAuth";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/registers' element={<Registers/>}></Route>
-        <Route path='/products' element={<Products/>}></Route>
-        <Route path='/login' element={<Login/>}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/registers" element={<Registers />}></Route>
+        <Route path="/products" element={<Products />}></Route>
+        <Route
+          path="/orders"
+          element={
+            <RequireAuth>
+              <Orders />
+            </RequireAuth>
+          }
+        ></Route>
+        <Route path="/login" element={<Login />}></Route>
       </Routes>
     </div>
   );
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { getAuth } from 'firebase/auth';
+import app from "../../firebase.init";
+
+
+const auth = getAuth(app);
 
 const Login = () => {
-
+   const [signInWithGoogle, user] = useSignInWithGoogle(auth)
   return (
     <div>
           <h3>Please Login Here</h3>
           <div style={{ margin: '20px' }}>
-              <button>Google Sign In</button>
+              <button onClick={()=> signInWithGoogle()}>Google Sign In</button>
           </div>
       <form>
         <input type="email" placeholder="Your Email" />
